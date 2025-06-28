e8c7a50a0c4423fe559d10b419c49c32153834ce
Rafael-J <r.martins.programmer@gmail.com>
2025-06-28 14:39:21 -0300

    Adição do método adicionarFuncionario na classe FuncionarioManager

--- src/com/rafael/gerenciamentofuncionarios/service/FuncionarioManager.java
+++ src/com/rafael/gerenciamentofuncionarios/service/FuncionarioManager.java
@@ -1,4 +1,15 @@
 package com.rafael.gerenciamentofuncionarios.service;
 
+import com.rafael.gerenciamentofuncionarios.model.Funcionario;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FuncionarioManager {
+    List<Funcionario> funcionarios = new ArrayList<>();
+
+    public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
 }
+
